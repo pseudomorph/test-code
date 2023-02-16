118f0038fc2518e6317bea760deca2465027813f
Ross Strickland <rkstrickland@gmail.com>
2023-02-16 10:42:44 -0600

    Update main.tf

--- test2/main.tf
+++ test2/main.tf
@@ -1,11 +1,5 @@
-provider "aws" {
-  skip_credentials_validation = true
-  skip_get_ec2_platforms      = true
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_requesting_account_id  = true
-}
-
-resource "aws_security_group" "name" {
-
+resource "null_resource" "localexec" {
+  provisioner "local-exec" {
+    command = "ls -la"
+  }
 }
